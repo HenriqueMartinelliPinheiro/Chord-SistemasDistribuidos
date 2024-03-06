af68f48dc49174eefeddde2a206b05a84d1a7ffe
Henrique Pinheiro <Henrique Pinheiro@DESKTOP-0GJUA0Q>
2024-03-06 20:06:01 -0300

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chord
+{
+    internal class Node
+    {
+        public int Id { get; }
+        public int Status { get; set; }
+        public Node? NextNode { get; set; }
+
+        private Dictionary<string, int> Resource; //hash, value
+        private Dictionary<int, int> NodeReference; // hashNode, idNode
+
+
+        public Node(int id, int status)
+        {
+            Id = id;
+            Resource = new Dictionary<string, int>();
+            Status = status;
+            NodeReference = new Dictionary<int, int>();
+            NextNode = null;
+        }
+
+        public void AddResource(int hash, string resource)
+        {
+            this.Resource.Add(resource, hash);
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + " Status: " + Status;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+
+namespace Chord
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] animals = {
+                "Cachorro", "Gato", "Cavalo", "Vaca", "Porco",
+                "Ovelha", "Galinha", "Pato", "Coelho", "Rato",
+                "Tigre", "Leão", "Elefante", "Girafa", "Macaco",
+                "Panda", "Lobo", "Raposa", "Urso", "Coala",
+                "Hipopótamo", "Crocodilo", "Tartaruga", "Cobra",
+                "Salamandra", "Sapo", "Rã", "Peixe", "Tubarão",
+                "Golfinho", "Baleia", "Pinguim", "Avestruz",
+                "Morcego", "Borboleta", "Abelha", "Formiga",
+                "Joaninha", "Libélula", "Lagarta"
+            };
+
+            int numberNodes = 10;
+            List<Node> nodes = new List<Node>();
+
+            for (int i = 0; i < numberNodes; i++)
+            {
+                int status = 0;
+
+                if (i % 5 == 0)
+                {
+                    status = 1;
+                }
+                nodes.Add(new Node(i + 1, status));
+            }
+
+            foreach (Node node in nodes)
+            {
+                Console.WriteLine(node.ToString());
+            }
+            FillResourcesDictionary(animals, numberNodes);
+        }
+        public static Dictionary<string, int> FillResourcesDictionary(string[] vect, int numberNodes)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            foreach (string animal in vect)
+            {
+                if (!dictionary.ContainsKey(animal))
+                {
+                    dictionary.Add(animal, GetHash(animal, numberNodes));
+                    Console.WriteLine("Hash: " + GetHash(animal, numberNodes) + " Animal: " + animal);
+                }
+            }
+            return dictionary;
+        }
+        private static int GetHash(string value, int numberNodes)
+        {
+            int h = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                h += value[i];
+            }
+            return (h*31)%numberNodes;
+        }
+    }
+
+}
