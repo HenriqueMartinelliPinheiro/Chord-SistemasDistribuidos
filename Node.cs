a3b06cb19ccea01ec66e1823b2f1b5dc4c9463ee
henriquePinheiro <48884506+HenriqueMartinelliPinheiro@users.noreply.github.com>
2024-03-08 09:12:35 -0300

    Update Node.cs

--- Node.cs
+++ Node.cs
@@ -11,28 +11,34 @@ namespace Chord
         public int Id { get; }
         public int Status { get; set; }
         public Node? NextNode { get; set; }
+        public Node? PreviousNode { get; set; }
 
-        public Dictionary<string,int> Resource { get; } //hash, value
-        private Dictionary<int, int> NodeReference; // hashNode, idNode
-
+        public Dictionary<string, int> Resource { get; } //hash, value
+        private List<Node> NodeReference { get;} // hashNode, idNode6
 
         public Node(int id, int status)
         {
             Id = id;
-            Resource = new Dictionary<string,int>();
+            Resource = new Dictionary<string, int>();
             Status = status;
-            NodeReference = new Dictionary<int, int>();
+            NodeReference = new List<Node>();
             NextNode = null;
+            PreviousNode = null;
         }
 
         public void AddResource(int hash, string resource)
         {
-            this.Resource.Add(resource, hash);
+            Resource.Add(resource, hash);
         }
 
         public override string ToString()
         {
             return "ID: " + Id + " Status: " + Status;
+        }
+
+        public void AddNodeReferencence(Node node)
+        {
+            NodeReference.Add(node);
         }
     }
 }
